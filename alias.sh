960cc9dc287c93bf81dc9c2b1835d485f2e8de3a
JavierMolines <eljavimolines@gmail.com>
2022-07-23 10:02:58 -0300

    Add Alias Bash File

---
+++ alias.sh
@@ -0,0 +1,21 @@
+# READ FILES
+alias ls="ls --color"
+alias xl="ls -al"
+
+# MOVEMENT BETWEEN FOLDERS
+alias gts="cd ~/Desktop/js/sancrisoft/"
+alias gtj="cd ~/Desktop/js/javier/"
+alias gtb="cd ~/Desktop/bash/"
+alias gth="cd ~/"
+
+# GIT COMMANDS
+alias ggpu="git push"
+alias ggp="git pull"
+alias ggt="git status"
+alias gga="git add ."
+alias ggm="git commit -m"
+
+# OTHERS
+alias rrbash="source ~/.bashrc"
+alias vs="code ."
+alias cat="read_file"
