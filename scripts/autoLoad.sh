18831598337713c082dc9bdff9b8f77ef3b9bc32
JavierMolines <eljavimolines@gmail.com>
2022-12-04 18:31:24 -0300

    Add autoload kitty.conf

--- scripts/autoLoad.sh
+++ scripts/autoLoad.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 host="$( pwd )"
+whojav="/home/$( whoami )"
 loadScriptVar="\n# COMMAND CUSTOM AUTOLOAD\n"
 loadScriptVar+="source $host/EnvLinux/scripts/alias.sh\n"
 loadScriptVar+="source $host/EnvLinux/scripts/myFunctions.sh\n"
 
-echo -e "$loadScriptVar" >> "/home/$( whoami )/.zshrc" && echo -e "\t> Config and files load Successfully, restart the terminal."
+echo -e "$loadScriptVar" >> "$whojav/.zshrc" 
+
+if [[ $? -eq 0 ]]
+then
+    rm -rf $whojav/.config/kitty && ln -s -f $host/EnvLinux/kitty $whojav/.config/kitty
+    if [[ $? -eq 0 ]]
+    then
+        echo -e "-> Config and files load Successfully, restart the terminal."
+    fi
+fi
