6e4750452020fd44ebf6ec87eb562d35f3e62402
JavierMolines <eljavimolines@gmail.com>
2023-09-26 06:55:48 -0300

    Update alias

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -1,17 +1,22 @@
 # MOVEMENT BETWEEN FOLDERS
-alias gtb="cd ~/Desktop/bash/"
-alias gtj="cd ~/Desktop/javier/"
-alias gtjs="cd ~/Desktop/javier/js/"
-alias gtky="cd ~/Desktop/bash/EnvLinux/"
-alias gtgo="cd ~/Desktop/javier/go/src"
+prefixRoute="cd ~/Desktop"
+
+alias gtb="$prefixRoute/bash/"
+alias gtj="$prefixRoute/javier/"
+alias gtjs="$prefixRoute/javier/js/"
+alias gtky="$prefixRoute/bash/EnvLinux/"
+alias gtgo="$prefixRoute/javier/go/src/"
 
 # GIT COMMANDS
-alias ggpu="git push"
-alias ggc="git checkout"
+alias ggc="git clone"
+alias ggu="git push"
 alias ggp="git pull"
 alias ggt="git status"
 alias gga="git add ."
 alias ggm="git commit -m"
+alias ggk="git checkout"
+
+# DOCKER COMMANDS
 alias doc="docker compose"
 alias kerrmi='docker rmi $(docker images -qf "dangling=true")'
 
