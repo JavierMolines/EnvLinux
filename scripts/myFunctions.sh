6027845fcae5b39b0adc1289fafa53ded6d2de1e
JavierMolines <eljavimolines@gmail.com>
2023-09-26 07:01:53 -0300

    Move commands from function to .txt

--- scripts/myFunctions.sh
+++ scripts/myFunctions.sh
@@ -9,11 +9,3 @@ stmf () {
   alias gtmf="cd $MAIN_FOLDER_ZSH"
   echo ">> Main Folder [$(echo $MAIN_FOLDER_ZSH)]"
 }
-
-# Comments
-
-# alias dock="sudo docker" # I define it this way so as not to create a docker group.
-# alias rrbash="source ~/.bashrc"
-# alias cat="read_file"
-# xdg-open -> open file manager
-# while read p; do echo "$p"; done < ~/Desktop/bash/EnvLinux/notes/zsh-theme-clean-custom.txt > ~/.oh-my-zsh/themes/clean.zsh-theme
