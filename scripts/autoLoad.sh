d4658fff3222752a88a0345b8b84bdf76f832450
Javier Molines <eljavimolines@gmail.com>
2024-01-13 09:25:45 -0300

    Better config to loadScript

--- scripts/autoLoad.sh
+++ scripts/autoLoad.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-echo -e "-> EnvLinux load config..."
+echo "-> EnvLinux load config..."
 
-host="$( pwd )"
-whojav="/home/$( whoami )"
-loadScriptVar="\n# COMMAND CUSTOM AUTOLOAD\n"
-loadScriptVar+="source $host/EnvLinux/scripts/alias.sh\n"
-loadScriptVar+="source $host/EnvLinux/scripts/myFunctions.sh\n"
+host="$(pwd)"
+whojav="/home/$(whoami)"
+
+loadScriptVar="
+# CONFIG ZSH
+export ZSH="\$HOME/.oh-my-zsh"
+ZSH_THEME="clean"
+plugins=(git zsh-syntax-highlighting)
+source \$ZSH/oh-my-zsh.sh
+
+# GO
+export GOPATH=\$HOME/Source/go
+export GOBIN=\$GOPATH/bin
+export GOROOT=/usr/lib/golang
+  
+export PATH=\$PATH:\$GOBIN:\$GOROOT/bin
+
+# COMMAND CUSTOM AUTOLOAD
+source $host/EnvLinux/scripts/alias.sh
+source $host/EnvLinux/scripts/myFunctions.sh
+"
 
 echo -e "$loadScriptVar" >> "$whojav/.zshrc" 
 
@@ -15,6 +31,6 @@ then
     rm -rf $whojav/.config/kitty && ln -s -f $host/EnvLinux/kitty $whojav/.config/kitty
     if [[ $? -eq 0 ]]
     then
-        echo -e "-> Config and files load Successfully, restart the terminal."
+        echo "-> Config and files load Successfully, restart the terminal."
     fi
 fi
