4e46de96bb39638e192311305d7dc9dd6a58d555
JavierMolines <eljavimolines@gmail.com>
2024-06-24 20:19:24 -0300

    Remove alias ohmyzsh git

--- scripts/autoLoad.sh
+++ scripts/autoLoad.sh
@@ -9,7 +9,7 @@ loadScriptVar="
 # CONFIG ZSH
 export ZSH="\$HOME/.oh-my-zsh"
 ZSH_THEME="clean"
-plugins=(git zsh-syntax-highlighting)
+plugins=(zsh-syntax-highlighting)
 source \$ZSH/oh-my-zsh.sh
 
 # GO
